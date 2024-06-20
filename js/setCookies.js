fd98e3554dacbeaeb6ce40b156d53bd5d7478843
tatiematie <iamtatiematie@gmail.com>
2024-06-20 16:49:43 -0700

    halved cookie expiration time

--- js/setCookies.js
+++ js/setCookies.js
@@ -1,6 +1,7 @@
-const setCookies = (key, keyName) => {
+// set cookies on user interaction
+const setCookies = (key) => {
     const cookieExpiration = new Date()
-        cookieExpiration.setDate(cookieExpiration.getDate() + 28)
+        cookieExpiration.setDate(cookieExpiration.getDate() + 14)
     const expires = cookieExpiration.toUTCString()
 
     document.cookie = `lastViewed=${key}; expires=${expires}; SameSite=Strict`
