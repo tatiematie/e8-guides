0712fd05a42bc4c0c896adb193ad8dc2e7f61d99
tatiematie <iamtatiematie@gmail.com>
2024-08-17 23:47:28 -0700

    item icon changes

--- js/buildPage.js
+++ js/buildPage.js
@@ -1,6 +1,5 @@
 import keyToID from "./keyToID.js"
 import updatePage from "./updatePage.js"
-import { getCookie, setCookie } from "./cookieHandling.js"
 
 // import data to build page on load
 const buildPage = () => {
@@ -13,13 +12,18 @@ const buildPage = () => {
 const newItemSelector = (key, values) => {
     const newListItem = document.createElement("li"),
         newButton = document.createElement("button"),
-        newImg = document.createElement("img")
+        newImg = document.createElement("img"),
+        newBGImg = document.createElement("img")
+
+    newBGImg.src = `assets/images/treatments/${values.rarity.toLowerCase()}.png`
+    newBGImg.classList.add("background")
+    newButton.appendChild(newBGImg)
 
     newImg.src = `assets/images/items/${keyToID(key)}.png`
+    newImg.classList.add("itemImg")
     newButton.appendChild(newImg)
 
     newButton.type = "button"
-    newButton.classList.add(values.rarity.toLowerCase())
     newButton.classList.add("item")
     newButton.title = key
     // newButton.id = keyToID(key)
