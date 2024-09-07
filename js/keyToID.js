a41a9162178862d8ce52bd337a33a8b6eb0fba8b
tatiematie <iamtatiematie@gmail.com>
2024-09-06 18:51:23 -0700

    removed in rework

--- js/keyToID.js
+++
@@ -1,7 +0,0 @@
-// function to convert item names to url readable ids
-const keyToID = (key) => {
-    let newKey = key.replace(/ /g, '-').replace(/'/g, '').replace(/\./g, '').toLowerCase()
-    return newKey
-}
-
-export default keyToID
