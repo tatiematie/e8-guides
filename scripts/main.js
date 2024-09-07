080c0a76f0f9ab69901c1a257918ec41158ab9b7
tatiematie <iamtatiematie@gmail.com>
2024-09-06 18:47:25 -0700

    complete rework of how pages are loaded

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+import buildSelect from "./buildSelect.js"
+import { getCookie, setCookie } from "./cookieHandling.js"
+import loadData from "./loadData.js"
+import resizeSelect from "./resizeSelect.js"
+import updateDisplay from "./updateDisplay.js"
+
+const loadPage = async () => {
+	window.itemSelect = document.querySelector("#item-select")
+
+	// define global variable to store the list of items and their data
+	window.itemList = await loadData("data/itemData.json"),
+		window.itemIDs = Object.keys(window.itemList)
+
+	// build/initialize the item selection list
+	buildSelect()
+
+	// set up the page according according to how the user's last viewed item, if available
+	if (!getCookie("lastViewed")) {
+		setCookie("lastViewed", window.itemIDs[0], 7)
+	}
+
+	// update tab and page with data from cookies
+	updateDisplay(getCookie("lastViewed"))
+
+	// update the item selection list to fit items properly in the list for responsive/mobile viewing
+	window.addEventListener("resize", () => {
+		resizeSelect()
+	})
+}
+
+// initialize the page
+loadPage()
